b2f759dd6be07c66af97a346fd34e34354b22f05
Khoi Dao <khoidao55@gmail.com>
2021-04-28 18:57:20 -0700

    implemented contains method for BST

--- BST/BST.js
+++ BST/BST.js
@@ -1,9 +1,3 @@
-class BST {
-  constructor() {
-    this.root = null;
-  }
-}
-
 class Node {
   constructor(value) {
     this.value = value;
@@ -12,21 +6,67 @@ class Node {
   }
 }
 
-insert(value) {
-  if(!this.root) {
-    this.root = new Node(value);
-  } else {
-    let currentNode = this.root;
-    while(currentNode) {
-      if(currentNode.val < value && currentNode.left === null) {
-        currentNode.left = new Node(value);
-      } else if(currentNode.val < value && currentNode.left !== null) {
-        currentNode = currentNode.left;
-      } else if(currentNode.val > value && currentNode.right !== null) {
-        currentNode = currentNode.right;
-      } else {
-        currentNode.right = new Node(value);
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+    if(!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+      while(currentNode) {
+        if(currentNode.value > value) {
+          if(currentNode.left === null) {
+            currentNode.left = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.left;
+          }
+        } else {
+          if(currentNode.right === null) {
+            currentNode.right = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.right;
+          }
+        }
+      }
+    }
+  }
+
+  contains(value) {
+    if(!this.root) {
+      return false;
+    } else {
+      let currentNode = this.root;
+      while(currentNode) {
+        if(value === currentNode.value) {
+          return true;
+        } else if(value > currentNode.value && currentNode.right !== null) {
+          currentNode = currentNode.right;
+        } else if (value < currentNode.value && currentNode.left !== null) {
+          currentNode = currentNode.left;
+        } else {
+          return false;
+        }
       }
     }
   }
 }
+
+
+var tree = new BST();
+// tree.insert(10);
+// tree.insert(5);
+// tree.insert(2);
+// tree.insert(13);
+// tree.insert(7);
+// tree.insert(11);
+// tree.insert(16);
+
+//console.log(tree);
+console.log(tree.contains(1));
