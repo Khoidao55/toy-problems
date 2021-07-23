e39bad01a667f6f5b617ecc9513fd83745e21db6
Khoi Dao <khoidao55@gmail.com>
2021-07-23 14:08:56 -0700

    addEdge implemented

--- Graphs/Graph.js
+++ Graphs/Graph.js
@@ -1,5 +1,24 @@
-class Graph = {
+//Undirected Graph
+class Graph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  addVertex(vertex) {
+    if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertexOne, vertexTwo) {
+    if(!(this.adjacencyList[vertexOne].includes(vertexTwo))) this.adjacencyList[vertexOne].push(vertexTwo);
+    if(!(this.adjacencyList[vertexTwo].includes(vertexOne))) this.adjacencyList[vertexTwo].push(vertexOne);
+  }
 }
+
+
+const graph = new Graph();
+
+graph.addVertex("New York");
+graph.addVertex("San Francisco");
+graph.addVertex("Minnesota");
+graph.addEdge("New York", "San Francisco");
+console.log(graph);
