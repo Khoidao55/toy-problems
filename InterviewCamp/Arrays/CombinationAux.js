649362e7fcbb3104380ccdd38f11c3883d7b6bcd
Khoi Dao <khoidao55@gmail.com>
2021-08-02 17:41:21 -0700

    addec phone mnemonics

--- InterviewCamp/Arrays/CombinationAux.js
+++ InterviewCamp/Arrays/CombinationAux.js
@@ -3,6 +3,7 @@ print all combination of length 3
 */
 
 const combinationAux = (arr) => {
+  debugger;
   return printCombos(arr, [], 0, 0);
   //return answerArr;
 }
@@ -23,8 +24,47 @@ const printCombos = (arr, buffer, startingIdx, bufferIdx) => {
   }
 }
 
-console.log(combinationAux([1,2,3,4,5,6,7]));
+//onsole.log(combinationAux([1,2,3,4,5,6,7]));
 // i = 4
 // [1, , ]
 // [1, 2, ]
 // [1, 2, 3]
+
+const phoneMnemonics = (n) => {
+  let phoneLetters = {
+    '0': [],
+    '1': [],
+    '2': ['A', 'B', 'C'],
+    '3': ['D', 'E', 'F'],
+    '4': ['G', 'H', 'I'],
+    '5': ['J', 'K', 'L'],
+    '6': ['M', 'N', 'O'],
+    '7': ['P', 'Q', 'R', 'S'],
+    '8': ['T', 'U', 'V'],
+    '9': ['W', 'X', 'Y', 'Z']
+  }
+
+  solvePhoneMnemonics(n.split(''), [], 0, 0, phoneLetters);
+}
+
+const solvePhoneMnemonics = (n, buffer, nextIdx, bufferIdx, phoneLetters) => {
+  if(bufferIdx === 3 || nextIdx === n.length) {
+    console.log(buffer);
+    return;
+  }
+
+  if(nextIdx === n.length) {
+    return;
+  }
+
+  let letters = phoneLetters[n[nextIdx]];
+  if(letters.length === 0) {
+    solvePhoneMnemonics(n, buffer, nextIdx + 1, bufferIdx, phoneLetters);
+  }
+  for(let char of letters) {
+    buffer[bufferIdx] = char;
+    solvePhoneMnemonics(n, buffer, nextIdx + 1, bufferIdx + 1, phoneLetters);
+  }
+}
+
+console.log(phoneMnemonics('123'));
