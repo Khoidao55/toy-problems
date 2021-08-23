d8fe63a1fa5c54eda982a80c7d18806d5c90a1d3
Khoi Dao <khoidao55@gmail.com>
2021-08-22 17:03:33 -0700

    able to fill the board

--- LeetCode/sudokuSolver.js
+++ LeetCode/sudokuSolver.js
@@ -1,9 +1,25 @@
 const sudokuSolver = (board) => {
-  return recurseSudoku(0, 0, board);
+  for(let row in board) {
+    recurseSudoku(row, 0, board);
+  }
+  return board;
 }
 
-const recurseSudoku = () => {
+const recurseSudoku = (row, col, board) => {
+  if(col === 9) {
+    return board;
+  }
 
+  //validate both row, column, and square;
+
+  if(board[row][col] === '.') {
+    for(let i = 1; i <= 9; i++) {
+      board[row][col] = i;
+      recurseSudoku(row, col + 1, board);
+    }
+  } else {
+    recurseSudoku(row, col + 1, board);
+  }
 }
 
 const columnValidity = () => {
@@ -25,3 +41,5 @@ const board = [
   [".",".",".","4","1","9",".",".","5"],
   [".",".",".",".","8",".",".","7","9"]
 ]
+
+console.log(sudokuSolver(board));
