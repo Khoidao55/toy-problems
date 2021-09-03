28d2a470c1dd885704896ac430357efb2e137d14
Khoi Dao <khoidao55@gmail.com>
2021-09-02 17:15:59 -0700

    added my own solution for mergeIntervals

--- LeetCode/mergeIntervals.js
+++ LeetCode/mergeIntervals.js
@@ -12,3 +12,21 @@ var merge = function(intervals) {
   return overlappingArr;
 };
 
+// my solution
+var merge = function(intervals) {
+  intervals.sort((a, b) => a[0] - b[0])
+  const overlappingArr = [intervals[0]];
+  let pointerTwo = 1;
+
+  while(pointerTwo < intervals.length) {
+      let firstElement = overlappingArr[overlappingArr.length - 1];
+      let secondElement = intervals[pointerTwo][0];
+      if(firstElement[1] >= secondElement) {
+          firstElement[1] = Math.max(firstElement[1], intervals[pointerTwo][1]);
+      } else {
+          overlappingArr.push(intervals[pointerTwo]);
+      }
+      pointerTwo++;
+  }
+  return overlappingArr;
+};
