6ada83f7d964c98a1b930a6b387b7499640009da
Khoi Dao <khoidao55@gmail.com>
2021-09-10 23:37:01 -0700

    redoing longestSubstring

--- Grokking/SlidingWindow/longestSubstring.js
+++ Grokking/SlidingWindow/longestSubstring.js
@@ -1,30 +1,13 @@
 //Given a string with lowercase letters only, if you are allowed to replace no more than ‘k’ letters with any letter, find the length of the longest substring having the same letters after replacement.
 
 const longestSubstring = (str, k) => {
-  let longestSubstring = -Infinity;
-  let charObj = {};
-  let numOfRepeatingChar = 0;
-  let windowStart = 0;
-  for(let windowEnd = 0; windowEnd < str.length; windowEnd++) {
-    let currentChar = str[windowEnd];
-    if(!(currentChar in charObj)) {
-      charObj[currentChar] = 0;
-    }
-    charObj[currentChar]++;
-    numOfRepeatingChar = Math.max(numOfRepeatingChar, charObj[currentChar]);
 
-    if((windowEnd - windowStart + 1 - numOfRepeatingChar) > k) {
-      let startChar = str[windowStart];
-      windowStart++;
-      charObj[startChar]--;
-    }
-
-    longestSubstring = Math.max(longestSubstring, windowEnd - windowStart + 1);
-  }
-  return longestSubstring;
 }
 
-//sum: 4
-//char{ b: 1, c: 2}
-
+/* window: [b,c,c, b, b, b]
+obj { b: 4 c: 2}
+numOfRepearintChar = 4;
+windowStart = 2;
+windowEnd = 6;
+*/
 module.exports = longestSubstring;
