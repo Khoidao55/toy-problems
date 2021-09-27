3eb62860cafdb01f71ada472474cf3acd6d3b7c0
Khoi Dao <khoidao55@gmail.com>
2021-09-27 16:25:54 -0700

    O(N) time, O(N) space

--- LeetCode/arrChange.js
+++ LeetCode/arrChange.js
@@ -1,46 +1,48 @@
-// const arrayChange = (arr) => {
-//   let largestEl = 0;
-//   for(let i = 0; i < arr.length; i++) {
-//     largestEl = Math.max(largestEl, arr[i]);
-//   }
-//   const arrExcludingLargest = arr.filter(el => el !== largestEl);
-//   debugger;
-//   return findCombo(0, arrExcludingLargest, largestEl, 0);
-// }
-
-// const findCombo = (start, arr, largestEl, sum) => {
-//   if(sum === largestEl) return true;
-//   if(start === arr.length) return false;
-
-//   for(let i = start; i < arr.length; i++) {
-//     const possibleSum = sum + arr[start];
-//     if(possibleSum > largestEl) {
-//       return findCombo(start + 1, arr, largestEl, sum);
-//     } else {
-//       return findCombo(start + 1, arr, largestEl, possibleSum);
-//     }
-//   }
-// }
-
 const arrayChange = (arr) => {
   let largestEl = 0;
   for(let i = 0; i < arr.length; i++) {
     largestEl = Math.max(largestEl, arr[i]);
   }
   const arrExcludingLargest = arr.filter(el => el !== largestEl);
-
-  let travellingPointer = 0;
-  let sum = 0;
-
-  for(let i = travellingPointer; i < arrExcludingLargest.length; i++) {
-    let possibleSum = sum + arr[i];
-    if(possibleSum === largestEl) return true;
-    if(possibleSum > largestEl) continue;
-    else {
-      sum = possibleSum;
-    }
+  for(let i = 0; i < arrExcludingLargest.length; i++) {
+    if(findCombo(i, arrExcludingLargest, largestEl, 0)) return true;
   }
   return false;
 }
 
-console.log(arrayChange([3,1,6]));
+const findCombo = (start, arr, largestEl, sum) => {
+  if(sum === largestEl) return true;
+  if(start === arr.length) return false;
+
+  for(let i = start; i < arr.length; i++) {
+    const possibleSum = sum + arr[start];
+    if(possibleSum > largestEl) {
+      return findCombo(start + 1, arr, largestEl, sum);
+    } else {
+      return findCombo(start + 1, arr, largestEl, possibleSum);
+    }
+  }
+}
+
+// const arrayChange = (arr) => {
+//   let largestEl = 0;
+//   for(let i = 0; i < arr.length; i++) {
+//     largestEl = Math.max(largestEl, arr[i]);
+//   }
+//   const arrExcludingLargest = arr.filter(el => el !== largestEl);
+
+//   let travellingPointer = 0;
+//   let sum = 0;
+
+//   for(let i = travellingPointer; i < arrExcludingLargest.length; i++) {
+//     let possibleSum = sum + arr[i];
+//     if(possibleSum === largestEl) return true;
+//     if(possibleSum > largestEl) continue;
+//     else {
+//       sum = possibleSum;
+//     }
+//   }
+//   return false;
+// }
+
+console.log(arrayChange([2,2,1,6]));
