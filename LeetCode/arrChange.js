56e27ce2a72d280748cfa4edbf0bfc2ea7d4c878
Khoi Dao <khoidao55@gmail.com>
2021-09-29 19:28:34 -0700

    O(N) time, O(N) space

--- LeetCode/arrChange.js
+++ LeetCode/arrChange.js
@@ -4,45 +4,22 @@ const arrayChange = (arr) => {
     largestEl = Math.max(largestEl, arr[i]);
   }
   const arrExcludingLargest = arr.filter(el => el !== largestEl);
-  for(let i = 0; i < arrExcludingLargest.length; i++) {
-    if(findCombo(i, arrExcludingLargest, largestEl, 0)) return true;
-  }
-  return false;
+
+  return findCombo(arrExcludingLargest, largestEl, 0) ? true : false;
 }
 
-const findCombo = (start, arr, largestEl, sum) => {
-  if(sum === largestEl) return true;
-  if(start === arr.length) return false;
-
-  for(let i = start; i < arr.length; i++) {
-    const possibleSum = sum + arr[start];
-    if(possibleSum > largestEl) {
-      return findCombo(start + 1, arr, largestEl, sum);
-    } else {
-      return findCombo(start + 1, arr, largestEl, possibleSum);
-    }
+const findCombo = (arr, largestEl, sum) => {
+  if(sum === largestEl) {
+    return true;
+  }
+  if(sum >= largestEl)  return;
+  for(let i = 0; i < arr.length; i++) {
+    const n = arr[i];
+    const remaining = arr.slice(i + 1);
+    let isValid = findCombo(remaining, largestEl, sum + n);
+    if(isValid) return true;
   }
 }
 
-// const arrayChange = (arr) => {
-//   let largestEl = 0;
-//   for(let i = 0; i < arr.length; i++) {
-//     largestEl = Math.max(largestEl, arr[i]);
-//   }
-//   const arrExcludingLargest = arr.filter(el => el !== largestEl);
 
-//   let travellingPointer = 0;
-//   let sum = 0;
-
-//   for(let i = travellingPointer; i < arrExcludingLargest.length; i++) {
-//     let possibleSum = sum + arr[i];
-//     if(possibleSum === largestEl) return true;
-//     if(possibleSum > largestEl) continue;
-//     else {
-//       sum = possibleSum;
-//     }
-//   }
-//   return false;
-// }
-
-console.log(arrayChange([2,2,1,6]));
+console.log(arrayChange([2,3,1,6]));
