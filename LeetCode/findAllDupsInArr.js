cd1a67a272af4e8d0b1a0f618ba2f2dbd7aba731
Khoi Dao <khoidao55@gmail.com>
2021-10-05 23:00:27 -0700

    O(N) time, O(1) space

---
+++ LeetCode/findAllDupsInArr.js
@@ -0,0 +1,11 @@
+var findDuplicates = function(nums) {
+  const result = []
+  for (let i = 0; i < nums.length; i++){
+      const index = Math.abs(nums[i]) - 1
+      if (nums[index] < 0){
+          result.push(index + 1)
+      }
+      nums[index] = -Math.abs(nums[index])
+  }
+  return result
+};
