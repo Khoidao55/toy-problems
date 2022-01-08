5447d28ec85a8bcce48d682c4aa54b7bf3e481b9
Khoi Dao <khoidao55@gmail.com>
2022-01-07 16:55:18 -0800

    added toy problem

--- LeetCode/linkedListRandomNode.js
+++ LeetCode/linkedListRandomNode.js
@@ -38,3 +38,58 @@ Solution.prototype.getRandom = function () {
  * var obj = new Solution(head)
  * var param_1 = obj.getRandom()
  */
+
+//FASTER SOLUTION O(N) time O(1) space
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ */
+class Solution {
+  constructor(head) {
+    this.head = head;
+    this.size = 0;
+  }
+
+  getSize() {
+    this.size = 0;
+    let currentNode = this.head;
+    while (currentNode) {
+      this.size++;
+      currentNode = currentNode.next;
+    }
+    return this.size;
+  }
+
+  traverseArr(index) {
+    let pos = 0;
+    let currentNode = this.head;
+    while (currentNode) {
+      if (pos === index) {
+        return currentNode.val;
+      }
+      pos++;
+      currentNode = currentNode.next;
+    }
+  }
+}
+
+/**
+ * @return {number}
+ */
+Solution.prototype.getRandom = function () {
+  let randomElement = Math.floor(Math.random() * this.getSize());
+  return this.traverseArr(randomElement);
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(head)
+ * var param_1 = obj.getRandom()
+ */
