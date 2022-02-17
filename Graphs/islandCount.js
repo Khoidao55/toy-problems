9278f390be99e676a51bfe0f1a4bffc29a87f082
Khoi Dao <khoidao55@gmail.com>
2022-02-17 15:27:35 -0800

    redoing grokking

--- Graphs/islandCount.js
+++ Graphs/islandCount.js
@@ -2,9 +2,9 @@ const islandCount = (grid) => {
   // todo
   const visited = new Set();
   let islandCount = 0;
-  for(let row in grid) {
-    for(let col in grid[row]) {
-      if(DFS(grid, parseInt(row), parseInt(col), visited)) {
+  for (let row in grid) {
+    for (let col in grid[row]) {
+      if (DFS(grid, parseInt(row), parseInt(col), visited)) {
         islandCount++;
       }
     }
@@ -13,8 +13,13 @@ const islandCount = (grid) => {
 };
 
 const DFS = (grid, row, col, visited) => {
-  if(grid[row] === undefined || grid[row][col] === undefined || grid[row][col] === 'W') return false
-  if(visited.has(`${row},${col}`)) return false;
+  if (
+    grid[row] === undefined ||
+    grid[row][col] === undefined ||
+    grid[row][col] === "W"
+  )
+    return false;
+  if (visited.has(`${row},${col}`)) return false;
   visited.add(`${row},${col}`);
   DFS(grid, row - 1, col, visited);
   DFS(grid, row + 1, col, visited);
@@ -22,21 +27,19 @@ const DFS = (grid, row, col, visited) => {
   DFS(grid, row, col + 1, visited);
 
   return true;
-}
-
+};
 
 const grid = [
-  ['W', 'L', 'W', 'W', 'W'],
-  ['W', 'L', 'W', 'W', 'W'],
-  ['W', 'W', 'W', 'L', 'W'],
-  ['W', 'W', 'L', 'L', 'W'],
-  ['L', 'W', 'W', 'L', 'L'],
-  ['L', 'L', 'W', 'W', 'W'],
+  ["W", "L", "W", "W", "W"],
+  ["W", "L", "W", "W", "W"],
+  ["W", "W", "W", "L", "W"],
+  ["W", "W", "L", "L", "W"],
+  ["L", "W", "W", "L", "L"],
+  ["L", "L", "W", "W", "W"],
 ];
 
-islandCount(grid); // -> 3
-
+console.log(islandCount(grid)); // -> 3
 
 module.exports = {
-  islandCount
+  islandCount,
 };
--- Grokking/SlidingWindow/longestSubarrayOne.js
+++
@@ -1,9 +0,0 @@
-//Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s, find the length of the longest contiguous subarray having all 1s.
-
-const longestSubarrayOne = (arr, k) => {
-
-}
-
-//[0, 1, 0, 0, 0, 0, 1]
-
-//k=2 if window > k, count0 <= k
